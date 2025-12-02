348847ce6a1618c5a8337db2a84b4f04266d33e9
lsomacarrera-cell <l.somacarrera@gmail.com>
2025-12-02 22:10:49 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,38 @@
-self.addEventListener("install", () => {
+const CACHE_NAME = "peso-obesmedica-v1";
+const ASSETS = [
+  "./",
+  "./peso.html",
+  "./manifest.json",
+  "./service-worker.js",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./Obesmedica.png"
+  // Si tienes el aguacate:
+  // "./aguacate.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", () => {
-  console.log("Service Worker activo");
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", () => {});
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
